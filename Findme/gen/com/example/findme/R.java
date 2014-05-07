c776c75854dc40351ef8e2df5c3ee0aa198317f3
Guido Corazza <guido.corazza@outlook.com>
2014-05-07 17:30:38 -0300

    Mas cambios XD

--- Findme/gen/com/example/findme/R.java
+++ Findme/gen/com/example/findme/R.java
@@ -29,7 +29,8 @@ public final class R {
         public static final int ic_action_settings=0x7f020004;
         public static final int ic_drawer=0x7f020005;
         public static final int ic_launcher=0x7f020006;
-        public static final int rating_favorite_rojo=0x7f020007;
+        public static final int ic_scan_button=0x7f020007;
+        public static final int rating_favorite_rojo=0x7f020008;
     }
     public static final class id {
         public static final int action_settings=0x7f080032;
