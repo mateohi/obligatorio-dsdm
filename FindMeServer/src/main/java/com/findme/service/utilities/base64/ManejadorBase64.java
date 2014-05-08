bef74fce817e394152ae41cea29df8f646d5671e
Mateo Hermida <mateohermida@outlook.com>
2014-05-08 18:21:25 -0300

    update base64

--- FindMeServer/src/main/java/com/findme/service/utilities/base64/ManejadorBase64.java
+++ FindMeServer/src/main/java/com/findme/service/utilities/base64/ManejadorBase64.java
@@ -5,43 +5,43 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
-
 import org.apache.tomcat.util.codec.binary.Base64;
 
 public class ManejadorBase64 {
 
-	public static void base64AFile(String base64, String path, String type) throws Base64Exception {
-		Base64 decoder = new Base64();
-        
+    public static void base64AFile(String base64, String path, String type) throws Base64Exception {
+        Base64 decoder = new Base64();
+
         try {
-        	byte[] imageByte = decoder.decode(base64);
-        	
+            byte[] imageByte = decoder.decode(base64);
+
             ByteArrayInputStream bis = new ByteArrayInputStream(imageByte);
             BufferedImage image = ImageIO.read(bis);
             bis.close();
-            
+
             ImageIO.write(image, type, new File(path));
-        } catch (Exception e) {
-        	throw new Base64Exception(Base64Exception.DECODIFICAR);
         }
-	}
-	
-	public static String fileABase64(String path, String type) throws Base64Exception {
-		Base64 encoder = new Base64();
-		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		
-		try {
-			BufferedImage img = ImageIO.read(new File(path));
-			ImageIO.write(img, type, bos);
+        catch (Exception e) {
+            throw new Base64Exception(Base64Exception.DECODIFICAR);
+        }
+    }
+
+    public static String fileABase64(String path, String type) throws Base64Exception {
+        Base64 encoder = new Base64();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+        try {
+            BufferedImage img = ImageIO.read(new File(path));
+            ImageIO.write(img, type, bos);
             byte[] imageBytes = bos.toByteArray();
             String imageString = encoder.encodeToString(imageBytes);
             bos.close();
-            
+
             return imageString;
-		} catch (IOException e) {
-			throw new Base64Exception(Base64Exception.CODIFICAR);
-		}
-	}
+        }
+        catch (IOException e) {
+            throw new Base64Exception(Base64Exception.CODIFICAR);
+        }
+    }
 }
