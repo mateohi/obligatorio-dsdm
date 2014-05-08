610df70b33eb38f5f2011db3c3b62d9b8057375f
Mateo Hermida <mateohermida@outlook.com>
2014-05-08 18:03:26 -0300

    cambios

--- FindMeServer/src/main/java/com/findme/service/dataaccess/dao/MascotaDao.java
+++ FindMeServer/src/main/java/com/findme/service/dataaccess/dao/MascotaDao.java
@@ -3,11 +3,27 @@ package com.findme.service.dataaccess.dao;
 import com.findme.service.dataaccess.IMascotaDao;
 import com.findme.service.model.Mascota;
 import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 public class MascotaDao implements IMascotaDao {
 
     @Override
     public List<Mascota> getMascotas() {
+        Configuration cfg = new Configuration().configure("hibernate.cfg.xml");
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(cfg.getProperties()).build();
+        SessionFactory sfr = cfg.buildSessionFactory(serviceRegistry);
+        Session session = sfr.openSession();
+        Transaction tx = session.beginTransaction();
+
+        session.update(null);
+
+        tx.commit();
+        session.close();
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
---
+++ FindMeServer/src/main/java/com/findme/service/model/Notificacion.java
@@ -0,0 +1,78 @@
+package com.findme.service.model;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "Notificacion")
+public class Notificacion extends Persistent {
+
+    @Column(name = "Dueño")
+    private Usuario usuarioDueno;
+    @Column(name = "Informante")
+    private Usuario usuarioInformante;
+    @Column(name = "Mascota")
+    private Mascota mascota;
+    @Column(name = "Fecha")
+    @Temporal(TemporalType.DATE)
+    private Date fecha;
+    @Column(name = "Longitud")
+    private String longitud;
+    @Column(name = "Latitud")
+    private String latitud;
+
+    public Notificacion() {
+    }
+
+    public Usuario getUsuarioDueno() {
+        return usuarioDueno;
+    }
+
+    public void setUsuarioDueno(Usuario usuarioDueno) {
+        this.usuarioDueno = usuarioDueno;
+    }
+
+    public Usuario getUsuarioInformante() {
+        return usuarioInformante;
+    }
+
+    public void setUsuarioInformante(Usuario usuarioInformante) {
+        this.usuarioInformante = usuarioInformante;
+    }
+
+    public Mascota getMascota() {
+        return mascota;
+    }
+
+    public void setMascota(Mascota mascota) {
+        this.mascota = mascota;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(String longitud) {
+        this.longitud = longitud;
+    }
+
+    public String getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(String latitud) {
+        this.latitud = latitud;
+    }
+}
